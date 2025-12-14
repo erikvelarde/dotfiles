98b5936477e7b8c388bbc5adf77ede3cb06af661
Eric Velarde <erickvelarde@bitbucket.org>
2025-12-13 21:35:53 -0400

    Nvim backup

--- cfg/nvim/init.lua
+++ cfg/nvim/init.lua
@@ -4,3 +4,18 @@ require("config.lazy")
 -- set colorscheme
 require("config.set-colorscheme")
 require("config.languages-conf")
+
+-- for spring boot live reload
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--   pattern = "*.java",
+--   callback = function()
+--     -- Run 'mvn compile' silently, discard output
+--     vim.fn.system("mvn compile -q")
+--     -- Optional: Notify on success/failure
+--     if vim.v.shell_error == 0 then
+--       print("✓ Compiled successfully (Spring Boot DevTools should restart)")
+--     else
+--       print("✗ Compilation failed (check logs)")
+--     end
+--   end,
+-- })
--- cfg/nvim/lua/config/languages-conf/languages/c.lua
+++ cfg/nvim/lua/config/languages-conf/languages/c.lua
@@ -1,4 +1,12 @@
 require("lspconfig").clangd.setup({
+	-- cmd = {
+	--   "clangd",
+	--   "--background-index",
+	--   "--clang-tidy",
+	--   "--header-insertion=never",
+	--   "--query-driver=/usr/bin/clang*,/usr/bin/gcc*",
+	--   "--compile-commands-dir=build",
+	-- },
 	cmd = { "clangd", "--compile-commands-dir=build" },
 	on_attach = on_attach,
 	flags = lsp_flags,
